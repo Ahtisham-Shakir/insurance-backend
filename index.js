e8c4b63b82f6e27103070c32c5fc42e4c8d8750e
Ahtisham-Shakir <ahtishamshakir777@gmail.com>
2023-09-04 16:00:42 +0500

    changes for railway

--- index.js
+++ index.js
@@ -103,7 +103,9 @@ mongoose
   })
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(port);
+    app.listen(port, "0.0.0.0", () => {
+      console.log("Server is running on port: " + port);
+    });
   })
   .catch((err) => console.log(err));
 
